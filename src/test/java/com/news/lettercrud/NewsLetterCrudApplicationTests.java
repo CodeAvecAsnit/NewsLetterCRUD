ae8192175758765e86e121ab0faf9bb776142a2f
bohara10 <rajanbohara71@gmail.com>
2025-10-31 10:21:23 +0545

    Final test

--- src/test/java/com/news/lettercrud/NewsLetterCrudApplicationTests.java
+++ src/test/java/com/news/lettercrud/NewsLetterCrudApplicationTests.java
@@ -1,5 +1,9 @@
 package com.news.lettercrud;
 
+import com.news.lettercrud.Data.model.BaseAccount;
+import com.news.lettercrud.Data.model.CompanyAccount;
+import com.news.lettercrud.Data.model.UserAccount;
+import org.apache.catalina.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +12,23 @@ class NewsLetterCrudApplicationTests {
 
     @Test
     void contextLoads() {
+        BaseAccount baseAccount = new BaseAccount();
+        UserAccount userAccount= new UserAccount();
+        CompanyAccount companyAccount = new CompanyAccount();
+
+    }
+
+    BaseAccount transform(UserAccount userAccount){
+        return UserAccount;
+    }
+
+    @Test
+    void  findUserAccount(User user){
+        int x = 19;
+        Integer m = Integer.valueOf(String.valueOf(x));
+        findUserAccount(user);
+
+
     }
 
 }
